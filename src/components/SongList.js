64a752261c0655a3f9897ffac1302c0e9b762331
reshinto <terencekong2002@gmail.com>
2019-10-26 16:46:51 +0800

    Add continuous playback feature

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -4,17 +4,26 @@ import {getSongs, deleteSong} from "../redux/actions/songAction";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
 import Dialog from "@material-ui/core/Dialog";
 import EditIcon from "@material-ui/icons/Edit";
-import StarIcon from '@material-ui/icons/Star';
+import StarIcon from "@material-ui/icons/Star";
 import Button from "@material-ui/core/Button";
 import Tooltip from "@material-ui/core/Tooltip";
 import AddVideoForm from "./forms/AddVideoForm";
-// import SimpleExpansionPanel from "./SimpleExpansionPanel";
-// import YouTubeVideo from "./YouTubeVideo";
+import YouTube from "react-youtube";
 
 class SongList extends React.Component {
   state = {
     open: false,
     currentId: 0,
+    opts: {
+      height: "200",
+      width: "300",
+      playerVars: {
+        autoplay: 1,
+      },
+    },
+    videoId: "",
+    currentIndex: 0,
+    player: null,
   };
 
   componentDidMount() {
@@ -29,88 +38,146 @@ class SongList extends React.Component {
     await this.setState({open: false});
   };
 
-  handleTitleClick = id => {
-    const iframe = document.getElementById(`video${id}`);
+  handleTitleClick = async (id, videoIndex) => {
+    const {songs} = this.props;
     const text = document.getElementById(`text${id}`);
-    if (iframe.style.display === "none") {
-      iframe.style.display = "inline-block";
+    for (let i = 0; i < songs.length; i++) {
+      const _text = document.getElementById(`text${songs[i].id}`);
+      if (_text.style.color === "rgb(245, 0, 87)") {
+        _text.style.color = "white";
+      }
+    }
+    if (text.style.color === "white") {
       text.style.color = "#f50057";
-    } else {
-      iframe.style.display = "none";
-      text.style.color = "white";
+      await this.setState({
+        videoId: this.props.songs[videoIndex].video_link,
+        currentIndex: videoIndex,
+      });
+    }
+  };
+
+  onReady = async event => {
+    // access to player in all event handlers via event.target
+    await this.setState({player: event.target});
+  };
+
+  onPlayVideo = () => {
+    this.state.player.playVideo();
+  };
+
+  onPauseVideo = () => {
+    this.state.play.pauseVideo();
+  };
+
+  onChangeVideo = async () => {
+    const {songs} = this.props;
+    const {currentIndex} = this.state;
+    let last = 0;
+    for (let i = 0; i < songs.length; i++) {
+      const _text = document.getElementById(`text${songs[i].id}`);
+      if (_text.style.color === "rgb(245, 0, 87)") {
+        _text.style.color = "white";
+      }
+    }
+    if (songs !== null && songs !== undefined) {
+      if (songs.length > 0) {
+        if (currentIndex < songs.length) {
+          if (currentIndex === songs.length - 1) last = 1;
+          const text = document.getElementById(
+            `text${songs[currentIndex + 1 - last].id}`,
+          );
+          if (text.style.color === "white") {
+            text.style.color = "#f50057";
+          }
+          await this.setState({
+            videoId: songs[currentIndex + 1 - last].video_link,
+            currentIndex: currentIndex + 1 - last,
+          });
+        }
+      }
+    }
+  };
+
+  initializeFirstVideo = async () => {
+    const {currentIndex} = this.state;
+    const {songs} = this.props;
+    const text = document.getElementById(`text${songs[currentIndex].id}`);
+    if (songs.length > 0) {
+      text.style.color = "#f50057";
+      await this.setState({
+        videoId: songs[currentIndex].video_link,
+      });
     }
   };
 
   render() {
     const {songs} = this.props;
+    const {opts, currentIndex, videoId} = this.state;
+    if (videoId === "") this.initializeFirstVideo();
     return (
       <div>
-        {songs.length > 0
-          ? songs.map((song, i) => (
-              <div key={song.id}>
-                <h3>
-                  <Button
-                    id={`text${song.id}`}
-                    style={{color: "white"}}
-                    onClick={() => this.handleTitleClick(song.id)}
-                  >
-                    {song.title} - {song.artist}
-                  </Button>
-                  <Tooltip title="Like">
-                    <Button>
-                      <StarIcon fontSize="small" color="secondary" />
-                    </Button>
-                  </Tooltip>
-                  <Tooltip title="Edit">
+        {songs.length > 0 ? (
+          <>
+            <YouTube
+              videoId={videoId}
+              opts={opts}
+              onReady={this.onReady}
+              onPlay={this.onPlayVideo}
+              onPause={this.onPauseVideo}
+              onEnd={this.onChangeVideo}
+            />
+            <div style={{height: "200px", overflowY: "scroll"}}>
+              {songs.map((song, i) => (
+                <div key={song.id}>
+                  <h3>
                     <Button
-                      onClick={() => this.handleClickOpen(song.id)}
+                      id={`text${song.id}`}
+                      style={{color: "white"}}
+                      onClick={() => this.handleTitleClick(song.id, i)}
                     >
-                      <EditIcon fontSize="small" color="secondary" />
+                      {song.title} - {song.artist}
                     </Button>
-                  </Tooltip>
-                  <Dialog
-                    open={this.state.open}
-                    onClose={this.handleClose}
-                    aria-labelledby="form-dialog-title"
-                  >
-                    <AddVideoForm type="Edit" songId={this.state.currentId} />
-                  </Dialog>
-                  <Tooltip title="Delete">
-                    <Button
-                      onClick={async () => {
-                        await this.props.deleteSong(song.id);
-                        window.location.reload(false);
-                      }}
+                    <Tooltip title="Like">
+                      <Button>
+                        <StarIcon fontSize="small" color="secondary" />
+                      </Button>
+                    </Tooltip>
+                    <Tooltip title="Edit">
+                      <Button onClick={() => this.handleClickOpen(song.id)}>
+                        <EditIcon fontSize="small" color="secondary" />
+                      </Button>
+                    </Tooltip>
+                    <Dialog
+                      open={this.state.open}
+                      onClose={this.handleClose}
+                      aria-labelledby="form-dialog-title"
                     >
-                      <DeleteForeverIcon fontSize="small" color="secondary" />
-                    </Button>
-                  </Tooltip>
-                </h3>
-                <iframe
-                  style={{display: "none"}}
-                  id={`video${song.id}`}
-                  title={`${song.title}|${song.artist}|${song.id}`}
-                  className="songs"
-                  width="300"
-                  height="200"
-                  src={`${song.video_link}?fs=1
-                  &rel=0
-                  &modestbranding=1
-                  &enablejsapi=1
-                  &autoplay=1`}
-                  frameborder="0"
-                  allow="encrypted-media"
-                  allowfullscreen="true"
-                ></iframe>
-                {/*<SimpleExpansionPanel songId={song.id}/>*/}
-                <hr />
-              </div>
-            ))
-          : ""}
+                      <AddVideoForm type="Edit" songId={this.state.currentId} />
+                    </Dialog>
+                    <Tooltip title="Delete">
+                      <Button
+                        onClick={async () => {
+                          await this.props.deleteSong(song.id);
+                          window.location.reload(false);
+                        }}
+                      >
+                        <DeleteForeverIcon fontSize="small" color="secondary" />
+                      </Button>
+                    </Tooltip>
+                  </h3>
+                  <hr />
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
 }
+// <YouTubeVideo width="300" height="200" id="pDEe-eJ4MAg" />
 
 const mapStateToProps = state => {
   return {
